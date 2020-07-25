94dc94535a3faeaf948a2a49812a263875ab7a37
Abha Jha <jhaabha08@gmail.com>
2020-07-25 22:56:20 +0530

    added market routes

---
+++ routes/marketRoutes.js
@@ -0,0 +1,20 @@
+const express= require('express')
+const Stocks= require('../models/stocks');
+const marketRouter= express.Router();
+
+marketRouter.route('/')
+.get((req,res,next)=>{
+    
+    Stocks.find({})
+    .then((stocks)=>{
+        res.statusCode=200;
+        // res.setHeader('Content-Type','application/json');
+        // res.json(stocks);
+        res.render("market",{stocks:stocks});
+    })
+    .catch((err)=>{
+        next(err);
+    })
+})
+
+module.exports=marketRouter;
