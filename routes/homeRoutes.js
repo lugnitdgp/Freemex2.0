c7ac0651b636056ed2d8621f740674083c24e96f
Abha Jha <jhaabha08@gmail.com>
2020-07-30 21:52:27 +0530

    routes for all navbar routes except market

---
+++ routes/homeRoutes.js
@@ -0,0 +1,229 @@
+const express= require('express')
+const Stocks= require('../models/stocks');
+const playerStocks=require('../models/playerStock')
+const Player=require('../models/users')
+
+const homeRouter= express.Router();
+
+homeRouter.get('/',(req,res)=>{
+    if(!req.user)
+    {
+        res.render("landing",{event_started:true})
+    }
+    else
+    {
+        playerStocks.find({player:req.user.id})
+        .populate('player')
+        .populate('stock')
+        .then((playerstocks)=>{
+            res.render("portfolio",{player:req.user,player_stocks:playerstocks})
+        })
+        .catch(err=>console.log(err))
+    }
+});
+
+homeRouter.post('/buystock/',(req,res,next)=>{
+    response_data={}
+    response_data.code=1;
+    response_data.message='Some Error Occured'
+    // console.log(response_data)
+    try {
+        var requestedStockCode=req.body.code;
+        var requestedStockCount=parseInt(req.body.quantity);
+        // console.log(requestedStockCode,requestedStockCount)
+    } catch (error) {
+        res.json(response_data)
+    }
+    Stocks.findOne({code:requestedStockCode})
+    .then((stockObj)=>{
+        Player.findById(req.user.id)
+        .then((playerObj)=>{
+            // console.log("player:",playerObj.cash)
+            var availableMoney=playerObj.cash
+            var stockPrice= stockObj.price
+            // console.log("available ",availableMoney," stockprice",stockPrice)
+            if(availableMoney>(stockPrice * requestedStockCount) && requestedStockCount>0)
+            {
+                try {
+                    playerStocks.find({player:playerObj.id,stock:stockObj.id})
+                    .populate('player')
+                    .populate('stock')
+                    .then((playerStockList)=>{
+                        // console.log("playerstock",playerStockList)
+                        if(playerStockList.length)
+                        {
+                            var playerStock = playerStockList[0]
+                            playerStock.quantity=playerStock.quantity + requestedStockCount
+                            playerStock.invested+=stockPrice * requestedStockCount
+                            playerStock.save()
+                            .then((resp)=>{
+                                // console.log(res)
+                                newAvailableMoney= availableMoney - (stockPrice * requestedStockCount)
+                                playerObj.cash= newAvailableMoney
+                                playerObj.value_in_stocks=0
+                                playerStocks.find({player:playerObj.id})
+                                .populate('player')
+                                .populate('stock')
+                                .then((playerstocks)=>{
+                                    playerstocks.map((playerstock)=>{
+                                        console.log("this",playerObj.value_in_stocks+playerstock.stock.price)
+                                        playerObj.value_in_stocks+=playerstock.stock.price * playerstock.quantity
+                                    })
+                                    playerObj.save()
+                                    .then(resp=>{
+                                        console.log("saved player stocks after buying",resp)
+                                        response_data.code=0
+                                        response_data.message='Transaction Successful'
+                                        // console.log("cahnged:",response_data)
+                                        res.json(response_data)
+                                    }) 
+                                    .catch((err)=>next(err))  
+                                })
+                                .catch(err=>res.json(response_data))
+                            })
+                        }
+                        else
+                        {
+                            var playerStock= new playerStocks({
+                                player:playerObj.id,
+                                stock:stockObj.id,
+                                quantity:requestedStockCount,
+                            })
+                            // console.log("creating playerStock: ",playerStock);
+                            var oldInvested=playerStock.invested
+                            var newInvested=oldInvested+stockPrice * requestedStockCount
+                            playerStock.invested=newInvested
+                            // console.log("creating playerStock after invest: ",playerStock);
+                            playerStock.save()
+                            .then(resp=>{
+                                console.log("saved",resp)
+                                       newAvailableMoney= availableMoney - (stockPrice * requestedStockCount)
+                        playerObj.cash= newAvailableMoney
+                        playerObj.value_in_stocks=0
+                        playerStocks.find({player:playerObj.id})
+                        .populate('player')
+                        .populate('stock')
+                        .then((playerstocks)=>{
+                            playerstocks.map((playerstock)=>{
+                                console.log("this",playerObj.value_in_stocks+playerstock.stock.price)
+                                playerObj.value_in_stocks+=playerstock.stock.price * playerstock.quantity
+                            })
+                            playerObj.save()
+                            .then(resp=>{
+                                console.log("saved player stocks after buying",resp)
+                                response_data.code=0
+                                response_data.message='Transaction Successful'
+                                // console.log("cahnged:",response_data)
+                                res.json(response_data)
+                            }) 
+                            .catch((err)=>next(err))  
+                        })
+                        .catch(err=>res.json(response_data))
+                            })
+                        }
+                    })
+                } catch (error) {
+                    // console("err here")
+                    res.json(response_data)
+                }
+            }
+            else{
+                response_data.code=1;
+                response_data.message="Not enough cash"
+                res.json(response_data)
+            }
+        })
+    })
+    .catch(err=>(next(err)))
+})
+
+homeRouter.post('/sellstock/', (req,res,next)=>{
+    response_data={}
+    response_data.code=1
+    response_data.message='Some Error Occurred'
+
+    try {
+        requestedStockCode=req.body.code
+        requestedStockCount=parseInt(req.body.quantity)
+    } catch (error) {
+        res.json(response_data);
+        next(error);
+    }
+    Stocks.findOne({code:requestedStockCode})
+    .then((stockObj)=>{
+        Player.findById(req.user.id)
+        .then((playerObj)=>{
+            var availableMoney =playerObj.cash
+            var stockPrice= stockObj.price
+
+            playerStocks.find({player:playerObj.id,stock:stockObj.id})
+            .populate('player')
+            .populate('stock')
+            .then((playerStockList)=>{
+                if(playerStockList.length && requestedStockCount <= playerStockList[0].quantity)
+                {
+                    try {
+                        var playerStock= playerStockList[0]
+                        var initial_investment = playerStock.invested
+
+                        playerStock.quantity = playerStock.quantity - requestedStockCount
+                        playerStock.invested= stockPrice * playerStock.quantity
+                        playerStock.save()
+                        .then((resp)=>{
+                            console.log("saved",resp)
+                            newAvailableMoney= availableMoney + (stockPrice * requestedStockCount)
+                            playerObj.cash = newAvailableMoney
+
+                            playerObj.value_in_stocks=0
+
+                            playerStocks.find({player:playerObj.id})
+                            .populate('player')
+                            .populate('stock')
+                            .then((playerstocks)=>{
+                                console.log("here",playerstocks)
+                                playerstocks.map((playerstock)=>{
+                                    console.log("price:",playerstock.stock.price ,"qty:",playerstock.quantity)
+                                    playerObj.value_in_stocks += playerstock.stock.price * playerstock.quantity
+                                })
+                                playerObj.save()
+                                .then((resp)=>{
+                                    console.log("changing player stocks", resp);
+                                    response_data.code=0
+                                    response_data.message="Transaction Successful"
+                                    res.json(response_data)
+                                })
+                                .catch((err)=>{
+                                    console.log(err)
+                                    next(err);
+                                })
+                            })
+                            .catch(err=>{
+                                console.log(err)
+                                next(err)
+                            })
+                        });
+                    } catch (error) {
+                        console.log(err)
+                        res.json(response_data)
+                        next(err)
+                    }
+                }
+                else
+                {
+                    response_data.code=1
+                    response_data.message= "Cannot sell more than what you have!"
+                    
+                    console.log(err)
+                    res.json(response_data);
+                }
+            })
+        })
+    })
+    .catch((err)=>{
+        console.log(err)
+        res.json(response_data)
+        next(err)
+    })
+})
+
+module.exports=homeRouter
