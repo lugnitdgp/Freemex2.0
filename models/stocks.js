7b20f644293bf3341a3fbd56912ddef9ca00329a
Ubuntu <ubuntu@ip-172-31-86-37.ec2.internal>
2021-03-07 15:24:21 +0000

    player assets query changed for faster query

--- models/stocks.js
+++ models/stocks.js
@@ -213,14 +213,14 @@ if (count===0){
         }
         else{
             console.log("fixtures loaded")
-            setInterval(()=>{updateStock.update_stock_prices(Stocks)},process.env.UpdateIntervalSecond*1000);
+  //          setInterval(()=>{updateStock.update_stock_prices(Stocks)},process.env.UpdateIntervalSecond*1000);
         }
     })
 }
 else
 {
     console.log(count," fixtures loaded already");
-    setInterval(()=>{updateStock.update_stock_prices(Stocks)},process.env.UpdateIntervalSecond*1000);
+//    setInterval(()=>{updateStock.update_stock_prices(Stocks)},process.env.UpdateIntervalSecond*1000);
 }
 })
 .catch((err)=>{
